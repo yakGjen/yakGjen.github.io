797cbd439a6e6ea1e138c0bb2657bf0daf42e760
Evgeniy Yakichuk <yak.gjen@gmail.com>
2019-02-27 16:47:37 +0300

    added pushState function on main page

--- js/index.js
+++ js/index.js
@@ -16,12 +16,12 @@
   let classContentH6 = 'content-h6';
   let classContentH6P = 'content-h6-p';
   
-  window.history.pushState({page: "main"}, "main page", "");
+  window.history.pushState({page: "main"}, "main page", "main");
   
   document.onclick = function(event) {
     if (event.target.closest('.menu-home')) {
       toogleContent('main');
-      window.history.pushState({page: "main"}, "main page", "");
+      window.history.pushState({page: "main"}, "main page", "main");
     }
     if (event.target.closest('.main-btn') || event.target.closest('.menu-summary')) {
       toogleContent('summary');
